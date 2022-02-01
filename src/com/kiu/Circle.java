1e3640403a502116960daef654a8b2c37e63e372
Kiu-Kyu <minkiukiu@gmail.com>
2022-02-02 04:19:25 +0700

    1st

---
+++ src/com/kiu/Circle.java
@@ -0,0 +1,30 @@
+package com.kiu;
+
+public class Circle extends Shape{
+    private double radius;
+
+    public Circle() {
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public Circle(String color, boolean filled, double radius) {
+        super(color, filled);
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public String toString() {
+        return "A circle with radius: " + getRadius();
+    }
+}
