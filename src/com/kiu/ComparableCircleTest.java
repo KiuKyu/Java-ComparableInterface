8ca3542ffefa9a86ca4157b640535c6957c0a07e
Kiu-Kyu <minkiukiu@gmail.com>
2022-02-03 18:40:20 +0700

    test more

--- src/com/kiu/ComparableCircleTest.java
+++ src/com/kiu/ComparableCircleTest.java
@@ -4,10 +4,13 @@ import java.util.Arrays;
 
 public class ComparableCircleTest {
     public static void main(String[] args) {
-        ComparableCircle[] circles = new ComparableCircle[3];
+        ComparableCircle[] circles = new ComparableCircle[5];
         circles[0] = new ComparableCircle(4.7);
         circles[1] = new ComparableCircle(2.3);
         circles[2] = new ComparableCircle("cyan", true, 7.8);
+        circles[3] = new ComparableCircle(1.5);
+        circles[4] = new ComparableCircle("red", false, 9.3);
+
 
         System.out.println("Pre-sorted: ");
         for (ComparableCircle circle : circles) {
